849f4909af679f26bf9c6bca896eb56522f143cd
SeunOdusole180 <oluwaseun.odusole@ucdconnect.ie>
2024-10-29 08:04:27 +0000

    branch creation(name)

--- Player.java
+++ Player.java
@@ -9,6 +9,7 @@ public class Player {
     public Player() {
         playerCount++;
         this.playerNumber = playerCount;
+        this.score = 0; // Initialize score to 0
         enterPlayerName();
     }
 
@@ -39,20 +40,9 @@ public class Player {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of players: ");
-        int numPlayers = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
-
-        Player[] players = new Player[numPlayers];
-
-        for (int i = 0; i < numPlayers; i++) {
-            players[i] = new Player();
-        }
-
-        System.out.println("\nPlayer Information:");
-        for (Player player : players) {
-            player.displayPlayerInfo();
-        }
+        Player player = new Player();
+        player.displayPlayerInfo();
     }
+
+
 }
