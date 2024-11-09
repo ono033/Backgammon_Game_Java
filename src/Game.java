c2c97e826997b02b5d9ecc1709277ad5888826f2
onose <onosenikhide1@gmail.com>
2024-11-09 19:31:57 +0000

    src folder fixed

---
+++ src/Game.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Game {
+
+
+    public Game() {
+
+        Board board = new Board();
+        board.setUpBoard();
+        board.printBoard();
+
+    }
+
+
+    public void setupBoard(){
+
+    }
+
+    public void displayBoard() {
+
+    }
+
+}
