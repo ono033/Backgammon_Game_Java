0eb40af95430749ae8998a120696d5a5b24b35e6
SeunOdusole180 <oluwaseun.odusole@ucdconnect.ie>
2024-11-12 15:48:52 +0000

    Updated Board with Bar and Checker Methods

--- src/Board.java
+++ src/Board.java
@@ -2,34 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-
-
 public class Board {
     public static final int NUMBEROFPIPS = 24;
 
-
-   /* private .. pips
-   private pip?
-   private middle
-   private off/on
-   top pips/ bottom pips, left and right?
-    */
-
     private ArrayList<ArrayList<Checker>> pips;
-
-
-
+    private ArrayList<Checker> bar;
 
     public Board() {
-// initialise
-initialiseVariables();
-setUpBoard();
-
+        initialiseVariables();
+        setUpBoard();
     }
 
-
-    public void initialiseVariables(){
+    public void initialiseVariables() {
         pips = new ArrayList<>();
+        bar = new ArrayList<>();
         // Initialize each pip with a Stack
         for (int i = 0; i < NUMBEROFPIPS; i++) { // assuming a standard backgammon board with 24 pips
             ArrayList<Checker> pip = new ArrayList<>();
@@ -45,7 +31,6 @@ setUpBoard();
         }
     }
 
-
     public Checker getChecker(int pipIndex, int checkerIndex) {
         ArrayList<Checker> pip = getPip(pipIndex);
         if (checkerIndex >= 0 && checkerIndex < pip.size()) {
@@ -55,105 +40,99 @@ setUpBoard();
         }
     }
 
-
-    // input is the pip index to add to and checker you want to add to the pip
-    public void addCheckertoPip(int pipIndex, Checker checker){
-    getPip(pipIndex).add(checker);
-        // ono add something if full?
-
+    public void addCheckertoPip(int pipIndex, Checker checker) {
+        getPip(pipIndex).add(checker);
     }
 
-    public void addCheckerstoPip(String type, int pipIndex, int number){
-
-        for(int i=0;i<number;i++){
+    public void addCheckerstoPip(String type, int pipIndex, int number) {
+        for (int i = 0; i < number; i++) {
             Checker checker = new Checker(type);
-            addCheckertoPip(pipIndex,checker);
+            addCheckertoPip(pipIndex, checker);
         }
-
-
     }
 
+    //Code to add checkers to the bar
+    public void addCheckerToBar(Checker checker) {
+        bar.add(checker);
+    }
 
-
-        public void setUpBoard(){
-        addCheckerstoPip("X", 1,2);
-            addCheckerstoPip("X", 12,5);
-            addCheckerstoPip("X", 17,3);
-            addCheckerstoPip("X", 20 ,5);
-
-            addCheckerstoPip("O", 24,2);
-            addCheckerstoPip("O", 13,5);
-            addCheckerstoPip("O", 8,3);
-            addCheckerstoPip("O", 6 ,5);
+    public void addCheckersToBar(List<Checker> checkers) {
+        bar.addAll(checkers);
+    }
+    // Code to remove checkers from bar
+    public Checker removeCheckerFromBar() {
+        if (!bar.isEmpty()) {
+            return bar.removeLast();
         }
+        return null; // Return null if the bar is empty
+    }
+    //Code to check if bar is empty
+    public boolean isBarEmpty() {
+        return bar.isEmpty();
+    }
 
+    public void printBar() {
+        System.out.print("BAR: ");
+        for (Checker checker : bar) {
+            System.out.print(checker.toString() + " ");
+        }
+        System.out.println();
+    }
 
+    public void setUpBoard() {
+        addCheckerstoPip("X", 1, 2);
+        addCheckerstoPip("X", 12, 5);
+        addCheckerstoPip("X", 17, 3);
+        addCheckerstoPip("X", 20, 5);
 
-
-
+        addCheckerstoPip("O", 24, 2);
+        addCheckerstoPip("O", 13, 5);
+        addCheckerstoPip("O", 8, 3);
+        addCheckerstoPip("O", 6, 5);
+    }
 
     public void printBoard() {
-//addCheckertoPip(13, new Checker("X"));
         System.out.println("13--+---+---+---+---18   BAR   19--+---+---+---+---24  OFF");
 
-        for(int i= 0;i<5;i++) {
-
-
-
+        for (int i = 0; i < 5; i++) {
             for (int j = NUMBEROFPIPS / 2 + 1; j <= NUMBEROFPIPS; j++) { // Print pips 13-24
-
-                if(j==19){  // space for bar
-                    System.out.print("    ---" );
+                if (j == 19) {  // space for bar
+                    System.out.print("    ---");
                 }
 
-                Checker checker = getChecker(j,i);
-                if(checker!=null) {
-
+                Checker checker = getChecker(j, i);
+                if (checker != null) {
                     System.out.print(checker.toString());
-                }
-
-
-                else{
+                } else {
                     System.out.print("|");
                 }
                 System.out.print("---");
-
             }
-
             System.out.println();
         }
         System.out.println("-+--+---+---+---+---+- -    -  -+--+---+---+---+---+- ");
 
-        for(int i= 0;i<5;i++) {
-
-            for (int j = NUMBEROFPIPS / 2 ; j > 0; j--) { // Print pips 13-24
-
-                if(j==6){
-                    System.out.print("    ---" );
+        for (int i = 0; i < 5; i++) {
+            for (int j = NUMBEROFPIPS / 2; j > 0; j--) { // Print pips 1-12
+                if (j == 6) {
+                    System.out.print("    ---");
                 }
-                Checker checker = getChecker(j,i);
-                if(checker!=null) {
+                Checker checker = getChecker(j, i);
+                if (checker != null) {
                     System.out.print(checker.toString());
-                }
-                else{
+                } else {
                     System.out.print("|");
                 }
                 System.out.print("---");
-
             }
-
             System.out.println();
         }
         System.out.println("12--+---+---+---+---07   BAR   06--+---+---+---+---01  OFF");
-
+        printBar();
     }
-
 
     public static void main(String[] args) {
-       Board board = new Board();
-       board.printBoard();
-
+        Board board = new Board();
+        board.printBoard();
     }
-
-
 }
