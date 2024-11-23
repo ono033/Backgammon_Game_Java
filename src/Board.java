5e8fb12a2340bd1f8edd2b66e47cbcf7ae0b71ba
onose <onosenikhide1@gmail.com>
2024-11-23 12:21:29 +0000

    basic makeMove complete

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
-
+import java.util.Random;
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class Board {
     public static final int NUMBEROFPIPS = 24;
+
+    // Constants for accessing move details in ArrayList
+    private static final int MOVE_INDEX = 0;
+    private static final int SOURCE_PIP = 1;
+    private static final int DESTINATION_PIP = 2;
+    private static final int DICE_VALUE = 3;
+    private static final int MOVE_TYPE = 4;
 
 
    /* private .. pips
@@ -78,6 +86,22 @@ public class Board {
 
     }
 
+    public Checker removeCheckerfromPip(int pipIndex) {
+        if (pipIndex < 1 || pipIndex > NUMBEROFPIPS) {
+            System.out.println("Invalid pip index: " + pipIndex);
+            return null;
+        }
+
+        ArrayList<Checker> pip = getPip(pipIndex);
+
+        if (pip == null || pip.isEmpty()) {
+            System.out.println("No checkers to remove from pip: " + pipIndex);
+            return null;
+        }
+
+        return pip.remove(pip.size() - 1);
+    }
+
 
     public void setUpBoard() {
         addCheckerstoPip("X", 1, 2);
@@ -109,86 +133,6 @@ public class Board {
     }
 
 
-/*
-    public void printBoard( int playerNumber) {
-//addCheckertoPip(13, new Checker("X"));
-        int PIPOFFSET = 24;
-
-
-        System.out.println("13--+---+---+---+---18   BAR   19--+---+---+---+---24  OFF");
-
-        for(int i= 0;i<5;i++) {
-           // int start;
-            //int end;
-
-           int start = (NUMBEROFPIPS / 2 + 1);
-           int end =NUMBEROFPIPS;
-
-            if (playerNumber ==2){
-            start = 24 - start;
-            end = 24 - end;
-            }
-
-            // if player 2   
-
-            for (int j = (NUMBEROFPIPS / 2 + 1); j <= NUMBEROFPIPS; j++) { // Print pips 13-24
-
-                if(j==19){  // space for bar
-                    System.out.print("    ---" );
-                }
-
-                Checker checker = getChecker(j,i);
-                if(checker!=null) {
-
-                    System.out.print(checker.toString());
-                }
-
-
-                else{
-                    System.out.print("|");
-                }
-                System.out.print("---");
-
-            }
-
-            System.out.println();
-        }
-        System.out.println("-+--+---+---+---+---+- -    -  -+--+---+---+---+---+- ");
-
-
-        int start =NUMBEROFPIPS / 2;
-        int end = 0;
-
-        if (playerNumber ==2){
-            start = 25 - start;
-            end = 25 - end;
-        }
-
-
-        for(int i= 0;i<5;i++) {
-
-            for (int j = start ; j > end; j--) { // Print pips 13-24
-
-                if(j==6){
-                    System.out.print("    ---" );
-                }
-                Checker checker = getChecker(j,i);
-                if(checker!=null) {
-                    System.out.print(checker.toString());
-                }
-                else{
-                    System.out.print("|");
-                }
-                System.out.print("---");
-
-            }
-
-            System.out.println();
-        }
-        System.out.println("12--+---+---+---+---07   BAR   06--+---+---+---+---01  OFF");
-
-    }
-*/
 
     public void printBoard(int playerNumber) {
 
@@ -278,14 +222,14 @@ public class Board {
         int size = getPip(pipIndex).size();
         Checker topChecker = getChecker(pipIndex, size - 1);
 
-        if (topChecker != null) System.out.print("topchecker:" + topChecker.toString());
+       // if (topChecker != null) System.out.print("topchecker:" + topChecker.toString()); ono remove
 
         return topChecker;
     }
 
     public boolean compareCheckers(Checker checker1, Checker checker2) {
         if (checker1 == null || checker2 == null) {
-            System.out.println("One or both checkers are null, cannot compare.");
+         //   System.out.println("One or both checkers are null, cannot compare."); ono remove
             return false;
         }
         // Return true if both checkers are of the same type
@@ -299,7 +243,7 @@ public class Board {
         //ono add error check for wrong colour checker
         int playerDirection = playerDirection(playerNumber);
         int destinationPipindex = sourcePipindex + (playerDirection * numberofMoves);
-        System.out.println("dest:" + destinationPipindex);
+       // System.out.println("dest:" + destinationPipindex); ono remove
 
         //error check ono for ourtside of 0 and 24
 
@@ -312,7 +256,7 @@ public class Board {
 
         // ono rn
         if (sourcePip == null||sourcePip.isEmpty()) {
-            System.out.println("Source pip is empty or null.");
+           // System.out.println("Source pip is empty or null."); ono remove
             return MoveType.ILLEGAL;
         }
 
@@ -321,12 +265,12 @@ public class Board {
 
         // Validate checker ownership
         if (moveChecker == null || !isPlayerChecker(playerNumber, moveChecker)) {
-            System.out.println("No checker to move or checker does not belong to player.");
+          //  System.out.println("No checker to move or checker does not belong to player."); ono remove
             return MoveType.ILLEGAL;
         }
 
 
-        System.out.println("movechecker" + moveChecker.toString());
+       // System.out.println("movechecker" + moveChecker.toString()); ono remove
         boolean rightCheckertype = isPlayerChecker(playerNumber, moveChecker);
 //if no checker or not players checker on spot
 
@@ -346,7 +290,7 @@ public class Board {
             boolean sameChecker = compareCheckers(destinationTopChecker, moveChecker);
 
 
-            //destination emptpy
+            //destination empty
             if (destinationSize == 0) {
                 return MoveType.LEGAL;
             }
@@ -368,6 +312,18 @@ public class Board {
         //direction of player
     }
 
+    public void movePiptoPip(int sourceIndex, int destinationIndex){
+
+        Checker checker = removeCheckerfromPip(sourceIndex);
+        if (checker != null) {
+            System.out.println("No checker to move");
+        }
+        addCheckertoPip(destinationIndex, checker);
+
+
+
+    }
+
     public void testlegalMoves() {
         int playerNumber = 1;
         int[] rollResult = {4, 1, 5};
@@ -381,8 +337,6 @@ public class Board {
 
         ArrayList<ArrayList<Object>> legalMoves = new ArrayList<>();
 
-
-
         for (int moveCount : rollResult) {
             int sourceIndex = 1; // loop from 1 - 24 ono
         while(sourceIndex<NUMBEROFPIPS+1) {
@@ -394,7 +348,7 @@ public class Board {
 
                 MoveType moveType = isLegal(playerNumber, sourceIndex, moveCount);
                 int destinationIndex = sourceIndex + (moveCount * playerDirection(playerNumber));
-                System.out.println("Dice Moves:" + moveCount + "\nFrom" + sourceIndex + " to " + destinationIndex + " Is moveType:" + moveType);
+          //      System.out.println("Dice Moves:" + moveCount + "\nFrom" + sourceIndex + " to " + destinationIndex + " Is moveType:" + moveType); ono take out
 
                 if (moveType != MoveType.ILLEGAL) { //if (moveType == MoveType.LEGAL || moveType == MoveType.KNOCKOUT) {
 
@@ -411,28 +365,116 @@ public class Board {
             }
         }
 
+
+
         return legalMoves;
 
 
     }
 
 public void printLegalMoves(ArrayList<ArrayList<Object>> legalMoves) {
-       System.out.println("\n\nLegal Moves:");
+       System.out.println("\nLegal Moves:");
         for (ArrayList<Object> move : legalMoves) {
-            System.out.println("Move " + move.get(0) + ": From Pip " + move.get(1) + " to Pip " + move.get(2) + ", Dice Value: " + move.get(3) + ", Move Type: " + move.get(4));
-            System.out.println(move);
+          //  System.out.println("Move " + move.get(0) + ": From Pip " + move.get(1) + " to Pip " + move.get(2) + ", Dice Value: " + move.get(3) + ", Move Type: " + move.get(4));
+         //   System.out.println(move);
+            System.out.println("Move " + move.get(MOVE_INDEX) +
+                    ": From Pip " + move.get(SOURCE_PIP) +
+                    " to Pip " + move.get(DESTINATION_PIP) +
+                    "         Dice Value: " + move.get(DICE_VALUE) +
+                    ", Move Type: " + move.get(MOVE_TYPE));
+
         }
 }
 
 public void makeMove(ArrayList<ArrayList<Object>> legalMoves, int moveNumber){
 
+        if(moveNumber<1 || moveNumber>legalMoves.size()) {      //ono check if this line is right
+            System.out.println("Illegal Move");
+            return;
+        }
+
+        ArrayList<Object> move = legalMoves.get(moveNumber-1);
+        MoveType moveType =  (MoveType)move.get(MOVE_TYPE);
+        int sourceIndex = (int) move.get(SOURCE_PIP);
+        int destinationIndex = (int) move.get(DESTINATION_PIP);
+        if(moveType ==MoveType.ILLEGAL){ // Return if move is illegal
+            System.out.println("Illegal Move");
+            return;
+        }
+
+        if(moveType ==MoveType.LEGAL){
+            movePiptoPip(sourceIndex, destinationIndex);
+            System.out.println("Legal Move... Successfull");
+
+        }
+
+        if(moveType ==MoveType.KNOCKOUT){
+            Checker hitChecker = removeCheckerfromPip(destinationIndex); // removes opposite
+            // ono add to bar
+
+            movePiptoPip(sourceIndex, destinationIndex);
+            System.out.println("Knockout Move... Successfull");
+
+
+        }
+
+
+}
+
+    public void diceRoll() {
+
+        Random random = new Random();
+        // Roll two dice
+        int die1 = random.nextInt(6) + 1;
+        int die2 = random.nextInt(6) + 1;
+
+        // Display the result of each dice
+        if (die1==die2){
+            System.out.println("You rolled:");
+            System.out.println("Die 1: " + die1);
+            System.out.println("Die 2: " + die2);
+            System.out.println("You rolled a double!");
+            System.out.println("You can roll " + die1 +"-"+ die1 +"-"+ die1 +"-"+ die1);
+        }
+        else{
+            System.out.println("You rolled:");
+            System.out.println("Die 1: " + die1);
+            System.out.println("Die 2: " + die2);
+        }}
+
+public void takeTurn(int playerNumber, int[] diceRoll){//ono what do I add?
+
+    ArrayList<ArrayList<Object>> legalMoves = legalMoves(playerNumber, diceRoll);
+    System.out.println("\n Select Move: " );
+    printLegalMoves(legalMoves);
+
+    Scanner scanner = new Scanner(System.in);
+    int moveNumber = scanner.nextInt();
+
+    makeMove(legalMoves, moveNumber);
 
 
 
 }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
+    Board board = new Board();
+//    Checker checker = new Checker("X");
+  // board.addCheckertoPip(5,checker);
+    int[] rollresult = new int[3];
+    rollresult[0] = 1;
+    rollresult[1] = 2;
+    rollresult[2] = 6;
+
+    int playerNumber = 1;
+while(true) {
+    board.printBoard(playerNumber);
+    board.takeTurn(playerNumber, rollresult);
+
+}
+
+        /*
 Board board = new Board();
 board.setUpBoard();
 int[] rollresult = new int[3];
@@ -440,121 +482,8 @@ rollresult[0] = 1;
         rollresult[1] = 2;
         rollresult[2] = 6;
       //  board.legalMoves(2, rollresult );
-        board.printLegalMoves( board.legalMoves(2, rollresult ));
-
+        board.legalMoves(2, rollresult);
+*/
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*public ArrayList<ArrayList<Integer>> legalMoves(int numberOfMoves, int playerNumber) {
-
-        ArrayList<ArrayList<Integer>> validMoves = new ArrayList<>();
-        int direction = playerDirection(playerNumber); //?
-
-        for (int startPip = 1; startPip <= NUMBEROFPIPS; startPip++) {
-
-            int destinationPip = startPip + direction * numberOfMoves; //?
-
-            //loop thru pips
-            MoveType result = isLegal(playerNumber, startPip, numberOfMoves);
-
-            if (result == MoveType.LEGAL || result == MoveType.KNOCKOUT) {
-                ArrayList<Integer> move = new ArrayList<>();
-                move.add(startPip);         // Store the starting pip
-                move.add(destinationPip);   // Store the destination pip
-                move.add(result == MoveType.LEGAL ? 1 : 2);  // 1 for LEGAL, 2 for KNOCKOUT
-                validMoves.add(move);
-            }
-        }
-
-        return validMoves;
-
-        }
-
-  */
-
-/*
-    public static void main(String[] args) {
-        Board board = new Board();
-        board.setUpBoard();  // Ensure the board is correctly set up for the game
-
-        int playerNumber = 1;  // Specify the player number you want to check moves for
-        int numberOfMoves = 4;  // Specify the number of steps you want to check for each move
-        ArrayList<ArrayList<Integer>> moves = board.legalMoves(numberOfMoves, playerNumber);
-        printValidMoves(moves);
-    }
-
-
-
-
-
-
-    public static void main(String[] args) {
-        Board board = new Board();
-        board.setUpBoard(); // Make sure the board is set up with initial checkers
-
-        // Test move legality
-        // Assuming player 1 is moving checker from pip 1 to pip 5
-        System.out.println("Testing Move from Pip 24 to Pip 2 for Player 1:");
-        MoveType move1 = board.isLegal(1, 24, 1);
-        System.out.println("Move 1: " + move1);
-
-        // Testing move that should be illegal if no checker exists in source
-        System.out.println("Testing Move from Pip 8 to Pip 6 for Player 1 (no checker expected at source):");
-        MoveType move2 = board.isLegal(1, 8, 6);
-        System.out.println("Move 2: " + move2);
-
-        // Testing a move that results in a knockout
-        System.out.println("Testing Move from Pip 12 to Pip 13 for Player 2 (should be a knockout if enemy checker present):");
-        MoveType move3 = board.isLegal(2, 12,2 );
-        System.out.println("Move 3: " + move3);
-
-        // Testing a move that is out of bounds
-        System.out.println("Testing Move from Pip 24 to Pip 28 for Player 2 (out of bounds):");
-        MoveType move4 = board.isLegal(2, 24, 4);
-        System.out.println("Move 4: " + move4);
-
-        // Testing another legal move
-        System.out.println("Testing Move from Pip 17 to Pip 13 for Player 1:");
-        MoveType move5 = board.isLegal(1, 17, -4);
-        System.out.println("Move 5: " + move5);
-    }
-
-    /*public static void main(String[] args) {
-       Board board = new Board();
-       board.printBoard(1);
-
-        //board.printBoard(2);
-   Checker check = board.getTopCheckerfromPip(13);
-
-
-       Checker check2 = board.getTopCheckerfromPip(18);
-
-
-        Checker check62 = board.getTopCheckerfromPip(17);
-
-//check.printChecker();
-       // check2.printChecker();
-      //  check62.printChecker();
-
-
-
-    } */
-
-
 
